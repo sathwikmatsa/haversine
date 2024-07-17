65e3aaa552a2653b69845d7a565468e408252873
Sathwik Matsa <sathwikmatsa@gmail.com>
2024-07-17 15:45:41 +0530

    Remove unique identifiers
    Rust Variable Shadowing FTW

--- perf/perf-attributes/src/lib.rs
+++ perf/perf-attributes/src/lib.rs
@@ -1,6 +1,5 @@
-use proc_macro2::Span;
 use quote::quote;
-use syn::{parse_quote, Error, Expr, Ident, ItemFn, LitStr};
+use syn::{parse_quote, Error, Expr, ItemFn, LitStr};
 
 #[proc_macro_attribute]
 pub fn instrument(
@@ -10,20 +9,13 @@ pub fn instrument(
     let mut input = syn::parse_macro_input!(item as ItemFn);
     let fn_name = input.sig.ident.to_string();
     let trace_name = format!("fn::{}", fn_name);
-    let tracer_var = Ident::new(format!("_trace_{}", fn_name).as_str(), Span::call_site());
     let block = input.block.as_mut();
     block.stmts.insert(
         0,
-        parse_quote! { let #tracer_var = perf::ScopedTrace::new(#trace_name);},
+        parse_quote! { let __trace_fn = perf::ScopedTrace::new(#trace_name);},
     );
     let gen = quote! {#input};
     gen.into()
-}
-
-fn to_valid_ident_name_lossy(s: &str) -> String {
-    s.chars()
-        .map(|x| if x.is_alphanumeric() { x } else { '_' })
-        .collect()
 }
 
 #[proc_macro_attribute]
@@ -40,12 +32,8 @@ pub fn instrument_loop(
     }
     let loop_name = syn::parse_macro_input!(args as LitStr).value();
     let trace_name = format!("loop::{}", loop_name);
-    let tracer_var = Ident::new(
-        format!("_trace_{}", to_valid_ident_name_lossy(trace_name.as_str())).as_str(),
-        Span::call_site(),
-    );
     let gen = quote! {{
-        let #tracer_var = perf::ScopedTrace::new(#trace_name);
+        let __trace_loop = perf::ScopedTrace::new(#trace_name);
         #input
     }};
     gen.into()
